a309441297c88a279381ba98259d9121d666a3da
Leo Wang <liaowng@gmail.com>
2018-03-21 14:37:48 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const helloWorld = () => {
+    console.warn("Print sth");
+};
+
+const square = (input: number): number => {
+    return input * input
+};
+
+export default {
+    helloWorld, square
+};
