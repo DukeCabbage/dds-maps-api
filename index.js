663a7295cf98286c82edf9c5653ab9d2ce068714
Leo Wang <liaowng@gmail.com>
2018-03-29 16:22:58 -0700

    Better conform to flow standard. Logging

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+// @flow
+import Logger from "js-logger";
+
 import DDSMapsApi from "./lib/DDSMapsApi";
 import * as AuthInterceptor from "./lib/AuthInterceptor";
+
+const LogLevel = {
+    DEBUG: Logger.DEBUG,
+    INFO: Logger.INFO,
+    TIME: Logger.TIME,
+    WARN: Logger.WARN,
+    ERROR: Logger.ERROR,
+    OFF: Logger.OFF,
+};
 
 export {
     AuthInterceptor,
     DDSMapsApi,
-}
+    LogLevel,
+};
